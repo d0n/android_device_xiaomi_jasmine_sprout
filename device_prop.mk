b94265e5c2a85e81203dce807ca89ae9e8f5cfc3
Akshay Kakatkar <akshayakakatkaraa@gmail.com>
2019-03-03 16:08:33 +0530

    PA Bringup

---
+++ device_prop.mk
@@ -0,0 +1,53 @@
+#
+# Copyright (C) 2018 The Xiaomi-SDM660 Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# Adaptive Display
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.qcom.ad=1 \
+	ro.qcom.ad.calib.data=/system/etc/calib.cfg \
+	ro.qcom.ad.sensortype=2
+
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.vendor.audio.sdk.fluencetype=none \
+	persist.vendor.audio.fluence.voicecall=true \
+	persist.vendor.audio.fluence.voicerec=false \
+	persist.vendor.audio.fluence.speaker=true \
+	vendor.audio.adm.buffering.ms=6 \
+	vendor.audio.offload.track.enable=false
+	
+# LCD
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=360	
+	
+# Dual SIM
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.radio.multisim.config=dsds
+
+# Paper mode
+PRODUCT_PROPERTY_OVERRIDES += \
+	sys.jdi_nt36672_offset=9 \
+	sys.jdi_nt36672_length=45 \
+	sys.paper_mode_max_level=32 \
+	sys.tianma_nt36672_offset=12 \
+	sys.tianma_nt36672_length=46
