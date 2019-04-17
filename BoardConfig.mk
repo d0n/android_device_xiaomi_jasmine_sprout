f33d8b955117f267c6574c645cca924da86a30a0
d0n <d0n@janeiskla.de>
2019-04-17 22:33:22 +0200

    deleted  = android_filesystem_config.h   modified = BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,7 @@ BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 BOARD_USES_RECOVERY_AS_BOOT := true
 
 # Recovery
-TARGET_NO_RECOVERY := true
+#TARGET_NO_RECOVERY := true
 RECOVERY_VARIANT := twrp
 WITH_TWRP := true
 
@@ -51,8 +51,86 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 # Platform
 BOARD_VENDOR_PLATFORM := xiaomi-sdm660
 
+# A/B
+AB_OTA_UPDATER := true
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+TARGET_NO_RECOVERY := true
+
+# DT2W
+TARGET_TAP_TO_WAKE_NODE := "/proc/nvt_wake_gesture"
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm660
+TARGET_KERNEL_CONFIG := jasmine-d0nkern_defconfig
+
+# Platform
+TARGET_BOARD_PLATFORM := sdm660
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno512
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := sdm660
+TARGET_NO_BOOTLOADER := true
+TARGET_USES_UEFI := true
+
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+
+# Enable CPUSets
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += skip_override
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE    := 4096
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET     := 0x01000000
+
+# Partitions
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x04000000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+BOARD_HAS_REMOVABLE_STORAGE := true
+
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Recovery
+TARGET_RECOVERY_WIPE := $(DEVICE_PATH)/recovery/root/etc/recovery.wipe
+
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Vendor
+TARGET_COPY_OUT_VENDOR := vendor
+
+# TWRP Configuration
+AB_OTA_UPDATER := true
+TW_THEME := portrait_hdpi
+TW_INCLUDE_CRYPTO := true
+TW_MAX_BRIGHTNESS := 100
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_EXCLUDE_SUPERSU := true
+TW_EXTRA_LANGUAGES := true
+TW_NO_SCREEN_BLANK := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+
+TARGET_RECOVERY_DEVICE_MODULES += android.hardware.boot@1.0
+
 # Root Folders
-BOARD_ROOT_EXTRA_FOLDERS := persist bt_firmware dsp firmware persist
+BOARD_ROOT_EXTRA_FOLDERS := persist
 
 # Vendor Security patch level
 VENDOR_SECURITY_PATCH := 2019-01-01
