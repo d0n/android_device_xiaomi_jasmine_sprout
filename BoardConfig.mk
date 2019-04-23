ee1db3d1dc5ba31f9cf125c3b6279eaa2a4163c6
d0n <d0n@janeiskla.de>
2019-04-23 22:25:48 +0200

    modified = BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,6 +61,7 @@ ENABLE_CPUSETS := true
 ENABLE_SCHEDBOOST := true
 
 # Kernel
+KERNEL_MAKE_FLAGS :=
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm660
 TARGET_KERNEL_CONFIG := jasmine-d0nkern_defconfig
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
